5ff32db9004bd4e2e503dc0f7f265e9a206ef07a
Daniel Levett <dlevett@hotmail.co.uk>
2018-09-20 17:04:36 +0100

    Basic smooth scrolling buttons works

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 import './Carousel.css';
 
 let uniqueSlideId = 0;
+
+const ButtonNext = ({ onClick }) => <button onClick={onClick}>&lt;</button>
+const ButtonPrevious = ({ onClick }) => <button onClick={onClick}>&gt;</button>
 
 class Carousel extends Component {
     static propTypes = {
@@ -15,25 +18,56 @@ class Carousel extends Component {
             { id: uniqueSlideId++, text: 'Slide 2' },
             { id: uniqueSlideId++, text: 'Slide 3' },
             { id: uniqueSlideId++, text: 'Slide 4' },
-        ]
+        ],
+        currentSlideIndex: 0
     };
+
+    carouselTrack = createRef();
+
+    goToNextSlide = () => {
+        this.setState(({ slides, currentSlideIndex }) => ({
+            currentSlideIndex: Math.min(currentSlideIndex + 1, slides.length)
+        }));
+    }
+
+    goToPreviousSlide = () => {
+        this.setState(({ currentSlideIndex }) => ({
+            currentSlideIndex: Math.max(currentSlideIndex - 1, 0)
+        }));
+    }
+
+    componentDidUpdate() {
+        const track = this.carouselTrack.current;
+        const { currentSlideIndex } = this.state;
+        const currentSlide = track.children[currentSlideIndex];
+        const { left } = currentSlide.getBoundingClientRect();
+
+        console.log(currentSlide.getBoundingClientRect())
+
+        track.scrollLeft = track.scrollLeft + left;
+    }
 
     render() {
         const { slidesToShow } = this.props;
-        const { slides } = this.state;
+        const { slides, currentSlideIndex } = this.state;
         return (
             <div className="carousel">
-                {slides.map(({ id, text }) =>
-                    <div
-                        className="slide"
-                        key={id}
-                        style={{ minWidth: `${100 / slidesToShow}%` }}
-                    >
-                        <div className="slide__content">
-                            {text}
+                {currentSlideIndex}
+                <ButtonNext onClick={this.goToPreviousSlide}/>
+                <div className="carousel--track" ref={this.carouselTrack}>
+                    {slides.map(({ id, text }) =>
+                        <div
+                            className="slide"
+                            key={id}
+                            style={{ minWidth: `${100 / slidesToShow}%` }}
+                        >
+                            <div className="slide__content">
+                                {text}
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )}
+                </div>
+                <ButtonPrevious onClick={this.goToNextSlide}/>
             </div>
         )
     }
